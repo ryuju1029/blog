d3da588599833bad48d922f75aaf52fd2910758e
ryuju1029 <ryuju19971029@gmail.com>
2021-09-22 12:27:13 +0900

    header項目作成

---
+++ header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="style.css">
+<header>
+  <div class="header_content">
+    <div class="header_item">
+      <a class="greeting_messege">こんにちはさん</a>
+    </div>
+    <div class="header_item right_content">
+      <a>マイページ</a>
+    </div>
+    <div class="header_item right_content">
+      <a>一覧ページ</a>
+    </div>
+    <div class="header_item right_content">
+      <a>ログアウト</a>
+    </div>
+  </div>
+</header>
